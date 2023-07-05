d250cf5cb1290498ab3da2141600def391f6a684
64657 <136047808+64657@users.noreply.github.com>
2023-07-05 11:54:29 +0530

    commit

---
+++ day 5/3006.js
@@ -0,0 +1,81 @@
+// const json = {
+//     name: "John",
+//     age: 30,
+//     city: "New York"
+//   };
+//   for (let key in json) {
+//     console.log(key + ": " + json[key]);
+//   }
+
+
+
+//   const person = {
+//     name: "John",
+//     age: 30,
+//     city: "New York"
+//   };
+//   const keys = Object.keys(person);
+//   for (let key of keys) {
+//     console.log(key + ": " + person[key]);
+//   } 
+
+  
+
+//   const person = {
+//     name: "John",
+//     age: 30,
+//     city: "New York"
+//   };
+//   Object.keys(person).forEach(key => {
+//     console.log(key + ": " + person[key]);
+//   });
+
+
+
+
+
+// {
+//     "name": "Mohammed Faseeullah",
+//     "email": "faseeullah.1998@gmail.com.com",
+//     "phone": "123-456-7890",
+//     "address": "123 Main Street",
+//     "education": [
+//       {
+//         "degree": "Bachelor of Engineering",
+//         "major": "Mechanical Engineering",
+//         "university": "Anna University",
+//         "year": 2016
+//       },
+//       {
+//         "degree": "Master of Engineering",
+//         "major": "Management",
+//         "university": "Anna University",
+//         "year": 2020
+//       }
+//     ],
+//     "experience": [
+//       {
+//         "position": "Piping Engineer",
+//         "company": "Designs Limited",
+//         "startDate": "2022-06-01",
+//         "endDate": "2023-12-31",
+        
+//       },
+  
+//     ],
+//     "skills": [
+//       "JavaScript",
+//       "React",
+//       "HTML",
+//       "CSS",
+//       "Python",
+//       "SQL",
+//       "Financial Analysis",
+//       "Budgeting"
+//     ]
+//   }
+
+
+
+
+  
